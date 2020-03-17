bf4ede86bf8f016168bb402ddf5099a2505a9b0b
Cory Hayes <corymhayes@gmail.com>
2020-03-17 14:28:44 -0500

    fix for zeit

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cd7350e2cf7a162b3136";
+/******/ 	var hotCurrentHash = "0ae9fd298f1edec0a232";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
